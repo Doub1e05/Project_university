ee10a9415efb9a682b97d59177154e4a3b4b11a5
Doub1e05 <tyfregde@gmail.com>
2024-04-01 01:57:39 +0300

    1 версия сканера

--- scaner/open_camera.py
+++ scaner/open_camera.py
@@ -4,7 +4,35 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.graphics.texture import Texture
+import easyocr
+from kivy.core.window import Window
 
+
+def text_recognition(file_path):
+    reader = easyocr.Reader(["ru"])
+    result = reader.readtext(file_path, detail=False)
+    digit_result = []
+    for i in result:
+        if i.isdigit():
+            digit_result.append(i)
+    return digit_result
+
+def main():
+
+    file_path = 'captured_image.jpg'
+    print("Recognizing text from the captured image...")
+
+    result = text_recognition(file_path)
+
+    print("Text recognition result:", result)
+
+def text_recognition(file_path):
+    reader = easyocr.Reader(["ru"])
+    result = reader.readtext(file_path, detail=False)
+    digit_result = []
+    for i in result:
+        if i.isdigit() and len(i) == 6: digit_result.append(i)
+    return  digit_result
 
 class CameraApp(App):
     def build(self):
@@ -25,12 +53,23 @@ class CameraApp(App):
     def capture(self, event):
         ret, frame = self.camera.read()
         if ret:
+
             frame = cv2.flip(frame, 0)
+
             buf1 = cv2.flip(frame, 0)
             buf = buf1.tostring()
             self.img_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+
             self.image_widget.texture = self.img_texture
+
             self.captured_image = frame
+
+            cv2.imwrite('captured_image.jpg', frame)
+            print("Фото успешно сохранено!")
+
+            main()
+
+            # App.get_running_app().stop()
         else:
             print("Ошибка при считывании изображения с камеры")
 
--- scaner/scaner.py
+++ scaner/scaner.py
@@ -1,15 +1,23 @@
+import cv2
 import easyocr
+
 def text_recognition(file_path):
     reader = easyocr.Reader(["ru"])
-    result = reader.readtext(file_path,detail=False)
+    result = reader.readtext(file_path, detail=False)
     digit_result = []
     for i in result:
-        if i.isdigit(): digit_result.append(i)
-    return  digit_result
+        if i.isdigit():
+            digit_result.append(i)
+    return digit_result
 
 def main():
-    file_path = input("Enter a file path: ")
-    print(text_recognition(file_path=file_path))
+    # Путь к файлу, который будет передан на распознавание
+    file_path = 'captured_image.jpg'  # Путь к файлу, который будет передан на распознавание
+    print("Recognizing text from the captured image...")
+    # Распознавание текста на изображении
+    result = text_recognition(file_path)
+    # Вывод результатов
+    print("Text recognition result:", result)
 
 if __name__ == "__main__":
     main()
