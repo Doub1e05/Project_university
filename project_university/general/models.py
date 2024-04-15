ff4c4681ca96c5759e7619737cca0eab3290e601
Doub1e05 <tyfregde@gmail.com>
2024-04-15 21:06:00 +0300

    Добавлена база данных

--- project_university/general/models.py
+++ project_university/general/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class generalForStudent(models.Model):
+    idOfStudent = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    nameOfStudent = models.CharField(max_length=255)
+    statusOfLab = models.BooleanField(default=False)
 
-# Create your models here.
+class generalForTeacher(models.Model):
+    idOfTeacher = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    nameOfTeacher = models.CharField(max_length=255)
+    nameOfDiscipline = models.CharField(max_length=255)
+    statusOfLab = models.BooleanField(default=False)
