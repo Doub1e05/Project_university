b73e3411d0402d7eee38703303d39d8a2864dded
Ar1hant <aganindanil@mail.ru>
2024-04-24 19:44:26 +0300

    Убрано приложение users. Созданы таблицы Авторизация и Потоки

--- project_university/project_university/urls.py
+++ project_university/project_university/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path
 
 urlpatterns = [
-    re_path('^admin/', admin.site.urls),
-    path('', include('general.urls')),
-    path('', include('users.urls')),
+    path('admin/', admin.site.urls),
 ]
