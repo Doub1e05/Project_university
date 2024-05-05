9769d2344af0658bfaa3af0765a50f3e95c25881
Ar1hant <aganindanil@mail.ru>
2024-05-05 21:26:25 +0300

    Фиксы

--- telegram_bot/Bot/local_settings.py
+++ telegram_bot/Bot/local_settings.py
@@ -5,4 +5,4 @@ API_KEY = "7163344457:AAEGvZTgYkA7DJqR-eovQiWhgUTqIrWgRO0"
 BASE_API_URL = "http://127.0.0.1:8000/api/work-statuses/"
 
 # Директория к базе данных
-DB_DIRECTORY = "C:/Users/Ar1hant/Desktop/New folder (2)/Project_university/project_university/db.sqlite3"
+DB_DIRECTORY = "C:/Users/Ar1hant/Desktop/Project/Project_university/project_university/db.sqlite3"
